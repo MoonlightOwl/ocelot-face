8fb74a2f508f988283ec56fec2e31d864ddd1471
MoonlightOwl <murky.owl@gmail.com>
2017-09-24 18:18:20 +0200

    allow to programmatically change terminal matrix

--- src/main/scala/totoro/ocelot/Ocelot.scala
+++ src/main/scala/totoro/ocelot/Ocelot.scala
@@ -1,7 +1,26 @@
 package totoro.ocelot
 
+import org.scalajs.jquery.jQuery
+
+/**
+  * Entry point of application
+  */
+
 object Ocelot {
+  private val greetings: String = "Come meow with us!"
+  private val matrix: Seq[StringBuilder] = List.fill(25)(
+    new StringBuilder("                                                                                ")
+  )
+
   def main(args: Array[String]): Unit = {
-    println("Come meow with us!")
+    // greet console hackers
+    println(greetings)
+    // greet users
+    set(2, 2, greetings)
+  }
+
+  private def set(x: Int, y: Int, string: String): Unit = {
+    matrix(y-1).replace(x-1, x-1 + string.length, string)
+    jQuery(s".l$y").text(matrix(y-1).mkString)
   }
 }
